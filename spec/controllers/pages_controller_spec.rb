9646fb26be25e9ab8edd0ed97c4e15ed08326ca8
Moses <yo_yeung@yahoo.com.hk>
2011-04-18 16:59:32 +0800

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => "Home")
     end
     
   end
@@ -24,7 +24,7 @@ describe PagesController do
      it "should have the right title" do
         get 'contact'
         response.should have_selector("title",
-                          :content => "Ruby on Rails Tutorial Sample App | Contact")
+                          :content => "Contact")
       end
     
   end
@@ -37,7 +37,7 @@ describe PagesController do
      it "should have the right title" do
         get 'about'
         response.should have_selector("title",
-                          :content => "Ruby on Rails Tutorial Sample App | About")
+                          :content => "About")
       end
     
   end
